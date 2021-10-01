b93876f9735917a7657553b718030aadc85bd6a6
ryuhanda <r.handa@everyleaf.com>
2021-10-01 12:11:57 +0900

    最終的な実行ファイルでのみ行っていた他クラスファイルの読み込みを、適宜必要なファイルで必要な分だけ行う様修正

--- 07.bowling_object/bowling.rb
+++ 07.bowling_object/bowling.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'shot'
-require_relative 'frame'
 require_relative 'game'
 
 game = Game.new(ARGV.first)
--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'frame'
+
 class Game
   def initialize(marks)
     @frames = formatted_marks(marks).map.with_index(1) do |formatted_marks, number|
